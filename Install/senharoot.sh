ee701da6cd1025f0a1b50a43a47a94cee85c0951
jailondanet2 <109008134+jailondanet2@users.noreply.github.com>
2022-07-28 21:00:59 -0300

    Update senharoot.sh

--- Install/senharoot.sh
+++ Install/senharoot.sh
@@ -6,10 +6,10 @@
 clear
 [[ "$(whoami)" != "root" ]] && {
 	clear
-	echo -e "\033[1;31mEJECUTE COMO USUARIO ROOT, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
+	echo -e "\033[1;31mEXECUTE COMO USUÁRIO ROOT, \033[1;32m(\033[1;33msudo -i\033[1;32m)\033[0m"
 	exit
 }
-echo -e "\n\033[1;32mPERMISOS DE AUTENTIFICACION ROOT... \033[0m"
+echo -e "\n\033[1;32mPERMISÕES DE AUTENTIFICAÇÃO ROOT... \033[0m"
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
@@ -37,12 +37,12 @@ iptables -A INPUT -p tcp --dport 8799 -j ACCEPT
 iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
 iptables -A INPUT -p tcp --dport 1194 -j ACCEPT
 sleep 3
-echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mPERMISOS APLICADOS ! \033[0m"
+echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mPERMISSÕES APLICADAS ! \033[0m"
 echo ""
-echo -ne "\033[1;32mESCOJA UNA NUEVA CONTRASEÑA ROOT\033[1;37m: "; read senha
+echo -ne "\033[1;32mESCOLHA UMA NOVA SENHA ROOT\033[1;37m: "; read senha
 [[ -z "$senha" ]] && {
-echo -e "\n\033[1;31mCONTRASEÑA INVALIDA !\033[0m"
+echo -e "\n\033[1;31mSENHA INVÁLIDA !\033[0m"
 exit 0
 }
 echo "root:$senha" | chpasswd
-echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mNUEVA CONTRASEÑA DEFINIDA ! \033[0m"
+echo -e "\n\033[1;31m[ \033[1;33mOK ! \033[1;31m]\033[1;37m - \033[1;32mNOVA SENHA  DEFINIDA ! \033[0m"
